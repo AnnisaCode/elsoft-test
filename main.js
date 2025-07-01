9f9513e4cb7e56e1f83711ff8e51977385265f26
Annisa <annisaseen@gmail.com>
2025-07-01 15:13:50 +0700

    Menambahkan fungsi showToast dan hideToast dari utils.js ke main.js untuk meningkatkan notifikasi toast. Menghapus implementasi lama dari main.js untuk menjaga kebersihan kode.

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 import { handleLogin, logout, showNavbar, hideNavbar } from './js/auth.js';
 import { loadItems, renderItems, renderPagination, filterItems, showItemModal, hideItemModal, handleItemSubmit, editItem, deleteItem, loadItemMasters, populateItemDropdowns } from './js/item.js';
 import { loadTransactions, renderTransactions, filterTransactions, showTransactionModal, hideTransactionModal, handleTransactionSubmit, editTransaction, deleteTransaction } from './js/transaction.js';
+import { showToast, hideToast } from './js/utils.js';
 
 // Global Variables
 let currentUser = null;
@@ -285,54 +286,6 @@ async function apiRequest(url, options = {}) {
     }
 }
 
-// Toast Notification Functions
-function showToast(message, type = 'info') {
-    const toast = document.getElementById('toast');
-    const toastContent = document.getElementById('toast-content');
-    const toastIcon = document.getElementById('toast-icon');
-    const toastMessage = document.getElementById('toast-message');
-
-    // Set message
-    toastMessage.textContent = message;
-
-    // Set icon and border color based on type
-    let iconHtml = '';
-    let borderClass = '';
-
-    switch (type) {
-        case 'success':
-            iconHtml = '<svg class="h-5 w-5 text-green-400" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>';
-            borderClass = 'border-green-400';
-            break;
-        case 'error':
-            iconHtml = '<svg class="h-5 w-5 text-red-400" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd"></path></svg>';
-            borderClass = 'border-red-400';
-            break;
-        case 'warning':
-            iconHtml = '<svg class="h-5 w-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clip-rule="evenodd"></path></svg>';
-            borderClass = 'border-yellow-400';
-            break;
-        default:
-            iconHtml = '<svg class="h-5 w-5 text-blue-400" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>';
-            borderClass = 'border-blue-400';
-    }
-
-    toastIcon.innerHTML = iconHtml;
-    toastContent.className = `bg-white shadow-lg rounded-lg p-4 max-w-sm border-l-4 ${borderClass}`;
-
-    // Show toast
-    toast.classList.remove('hidden');
-
-    // Auto hide after 5 seconds
-    setTimeout(() => {
-        hideToast();
-    }, 5000);
-}
-
-function hideToast() {
-    document.getElementById('toast').classList.add('hidden');
-}
-
 // Close modals when clicking outside
 window.onclick = function (event) {
     const itemModal = document.getElementById('item-modal');
@@ -359,4 +312,5 @@ window.editTransaction = editTransaction;
 window.deleteTransaction = deleteTransaction;
 window.hideTransactionModal = hideTransactionModal;
 window.handleTransactionSubmit = handleTransactionSubmit;
-window.loadTransactions = loadTransactions; 
+window.loadTransactions = loadTransactions;
+window.hideToast = hideToast; 
